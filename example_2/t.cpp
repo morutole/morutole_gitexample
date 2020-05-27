054ea6710b5e1bb2f1ebd91b74d6e44a7b2dc244
morutole <50069930+morutole@users.noreply.github.com>
2020-05-27 16:11:38 +0900

    Change to Hello World

--- example_2/t.cpp
+++ example_2/t.cpp
@@ -7,15 +7,6 @@ using ll = long long;
 
 int main()
 {
-    ll i,j;
-    ll n;
-    cin >> n;
-    vector<ll> a(n);
-    for(i = 0;i < n;++i){
-        cin >> a.at(i);
-    }
-    for(i = 0;i < n;++i){
-        cout << a.at(i) + 2 << endl;
-    }
+    cout << "Hello world" << endl;
     return 0;
 }
